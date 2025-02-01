54b7c94d7262cb8e9f006b7363e81949f9237120
DeeDash <rumpelstiltskin512@gmail.com>
2025-02-02 01:35:42 +0530

    Initial commit

---
+++ TicTacToe/tictactoe.js
@@ -0,0 +1,13 @@
+/* DOM MANIPULATION
+ * function to render board 
+ * function to display turn 
+ * based on turn, when block is clicked, input the player value
+ * render reset button when game is over, and then render board again
+ */
+
+/* GAME LOGIC
+ * switch turns
+ * current turn inputs corresponding cell
+ * if a[i][0..3] or a[0..3][j] or a[i..][j..] st i == j is the same, then that player wins
+ * can check for win after move 5, no need to check after every turn
+ */
