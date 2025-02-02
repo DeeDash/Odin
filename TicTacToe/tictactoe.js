24df711ee0549694776385720a91995849475d21
DeeDash <rumpelstiltskin512@gmail.com>
2025-02-02 21:36:09 +0530

    Console Game - Remove Comments

--- TicTacToe/tictactoe.js
+++ TicTacToe/tictactoe.js
@@ -1,17 +1,3 @@
-/* DOM MANIPULATION
- * function to render board 
- * function to display turn 
- * based on turn, when block is clicked, input the player value
- * render reset button when game is over, and then render board again
- */
-
-/* GAME LOGIC
- * switch turns
- * current turn inputs corresponding cell
- * if a[i][0..3] or a[0..3][j] or a[i..][j..] st i == j is the same, then that player wins
- * can check for win after move 5, no need to check after every turn
- */
-
 /* problems - 
  * can call restart game from console to cheat
  */
