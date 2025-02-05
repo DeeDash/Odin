8a28300ef96e649a22bf088a5edca6602669099e
DeeDash <rumpelstiltskin512@gmail.com>
2025-02-06 04:01:07 +0530

    "Move pages/ and sections/ to src/

--- docs/Restaurant/src/index.js
+++ docs/Restaurant/src/index.js
@@ -1,6 +1,6 @@
-import createHeader from '../sections/header';
-import createMain from '../sections/main';
-import createFooter from '../sections/footer';
+import createHeader from './sections/header';
+import createMain from './sections/main';
+import createFooter from './sections/footer';
 
 
 createHeader();
